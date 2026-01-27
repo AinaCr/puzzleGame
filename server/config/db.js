b7951a5a5f994f9160a4d110ab834ddf58f37fdd
AinaCR <cyrilleaina@gmail.com>
2026-01-27 09:16:07 +0300

    Mise à jour du projet

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,40 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise'; 
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
-const con = mysql.createConnection({
+
+// Ces 2 lignes servent à retrouver le chemin du fichier ca.pem
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // Indispensable (22395 dans ton cas)
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+   
+    ca: fs.readFileSync(path.join(__dirname, '../ca.pem')),
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
+// Petit test automatique
+pool.getConnection()
+  .then((connection) => {
+    console.log("✅ Connexion Aiven réussie !");
+    connection.release();
+  })
+  .catch(err => {
+    console.error("❌ ERREUR DÉTAILLÉE :");
+    console.error("Code :", err.code);       // Ex: 'ETIMEDOUT' ou 'ECONNREFUSED'
+    console.error("Message :", err.message); // Le texte de l'erreur
+    console.error("Erreur complète :", err);
+  });
 
-export default con;
+export default pool;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 
+import dotenv from 'dotenv'
+dotenv.config()
+console.log("Ma clé Host est :", process.env.DB_HOST);
+console.log("Mon Port est :", process.env.PORT);
 import express from 'express'    // Framework serveur backend
 import path from 'path'          // Gestion des chemins de fichiers
 import cors from 'cors'          // Autorisation Cross-Origin pour frontend
 import userRoutes from './routes/userRoutes.js';
 import { fileURLToPath } from 'url'  // Conversion d'URL en chemin de fichier
+import db from './config/db.js'
+
+
 
 // ======================= Initialisation du serveur =======================
 const app = express()
@@ -14,7 +21,7 @@ const __dirname = path.dirname(__filename)
 app.use(cors())               // Autorise toutes les requêtes cross-origin
 app.use(express.json())       // Parse automatiquement le JSON dans req.body
 app.use("/uploads",express.static(path.join(__dirname,"uploads")))
-const PORT = process.env.PORT || 3000            // Port d'écoute du serveur
+const PORT = process.env.PORT            // Port d'écoute du serveur
 
 app.use("/user", userRoutes);
 
@@ -33,5 +40,5 @@ app.use(express.static(path.join(__dirname,"../client/dist")))
 
 // ======================= Lancer le serveur ==============================
 app.listen(PORT,()=>{
-    console.log("http://localhost:3000")
+    console.log(`🚀 Serveur lancé sur : http://localhost:${PORT}`)
 })
